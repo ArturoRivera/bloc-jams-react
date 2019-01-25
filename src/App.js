ccea4fe69f3dd8573f1dea0095da2f6098cd851f
Arturo Rivera <a.rivera13@hotmail.com>
2019-01-24 18:44:33 -0800

    Album router implemented

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Link} from 'react-router-dom';
 import './App.css';
 import Landing from './components/Landing';
 import Library from './components/Library';
+import Album from './components/Album';
 
 class App extends Component {
   render() {
@@ -10,12 +11,14 @@ class App extends Component {
       <div className="App">
         <header>
           <Link to='/'>Landing </Link>
-          <Link to='/library'> Library</Link>
+          <Link to='/library'> Library </Link>
+          <Link to='/album'> Album</Link>
           <h1>Bloc Jams</h1>
         </header>
         <main>
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
+          <Route path="/album" component={Album} />
         </main>
       </div>
     );
